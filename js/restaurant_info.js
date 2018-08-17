7bd1bec445e539f04961dc2fa4380701a53b2db6
Sanjeev Saroy <saroy94@gmail.com>
2018-08-17 12:17:17 +0100

    Load the map in the restaurant page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2Fyb3l5IiwiYSI6ImNqa3d0a3N5ejAxZ3Eza3FydGpra3ZtbW4ifQ.FYsBYH9uzI_5Pbi1Hh-emw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
